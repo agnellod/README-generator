362434f3cbb32d74cfe5f39627662290e11c84dc
Dylan <agnello.dylan95@gmail.com>
2023-02-28 15:23:45 -0700

    adding readme

--- README.md
+++ README.md
@@ -1,20 +1,48 @@
-# project 7
-   ## descriptions
-   readme generator
-   ## Installation
-   use node to make a readme through the terminal
-   ## Usage
-   making readme
-   ## license
-  // maybe make function for different licenses
-   ## screen shot
-   C:\Users\Agnel\OneDrive\Pictures\Screenshots\Screenshot_20230204_031204.png
-   ## tests
-   none
-   ## questions 
-   google
-   ## username
-   agnelllod95
-   ## email
-   gmail.com
- 
+# Project-7
+
+## descriptions
+
+I made a README generator through the terminal and used node to make it work. It allows you to create a README file and select a license to attach to it.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+just use the terminal
+
+## Usage
+
+Creating a README file for your repository
+
+## license
+
+[MIT license](https://opensource.org/license/mit/)
+
+![](https://img.shields.io/badge/license-MIT-blue)
+
+MIT
+
+## screen shot
+
+will be adding a video
+
+## tests
+
+none
+
+## questions 
+
+Message me on GitHub
+
+## credits
+
+https://github.com/agnellod
+
+## email
+
+Agnello.dylan95@gmail.com
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,39 +1,71 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  
-}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license){
+    case "GNU":
+      return `[GNU license](https://opensource.org/license/gpl-3-0/)`
+    case "Apache":
+      return `[Apache license](https://opensource.org/license/apache-2-0/)`
+    case "MIT":
+      return `[MIT license](https://opensource.org/license/mit/)`
+    case "Mozilla":
+      return `[Mozilla license](https://opensource.org/license/mpl-2-0/)`
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  }
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
   return `# ${answers.projectTitle}
-   ## descriptions
-   ${answers.description}
-   ## Installation
-   ${answers.installation}
-   ## Usage
-   ${answers.usage}
-   ## license
-   ${answers.license}
-   ## screen shot
-   ${answers.screenShot}
-   ## tests
-   ${answers.tests}
-   ## questions 
-   ${answers.questions}
-   ## username
-   ${answers.username}
-   ## email
-   ${answers.email}
- 
+
+## descriptions
+
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## license
+
+${renderLicenseLink(answers.license)}
+
+![](https://img.shields.io/badge/license-${answers.license}-blue)
+
+${answers.license}
+
+## screen shot
+
+${answers.screenShot}
+
+## tests
+
+${answers.tests}
+
+## questions 
+
+${answers.questions}
+
+## credits
+
+${answers.username}
+
+## email
+
+${answers.email}
 `;
 }
 
